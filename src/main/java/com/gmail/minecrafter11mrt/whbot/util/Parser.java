f08e0ebd75f3f2eff3b8b632b7e7950de1677742
underscore11code <minecrafter11mrt@gmail.com>
2019-11-20 13:42:12 -0800

    save

--- src/main/java/com/gmail/minecrafter11mrt/whbot/util/Parser.java
+++ src/main/java/com/gmail/minecrafter11mrt/whbot/util/Parser.java
@@ -5,14 +5,24 @@ import java.util.HashMap;
 
 public class Parser {
     private String content;
-    Parser(Message message){
+    public Parser(Message message){
         content=message.getContent();
     }
-    Parser(String text){
+    public Parser(String text){
         content=text;
     }
-    HashMap<String,String> splitFlags(){
-        HashMap<String,String> flags;
-
+    public HashMap<String,String> splitFlags(){
+        HashMap<String,String> flags=new HashMap<>();
+        String[] argsSplit=content.split(" ");
+        for(String arg : argsSplit){
+            if(arg.startsWith("-")){
+                if(arg.contains(":")){
+                    flags.put(arg.split(":")[0],arg.split(":")[1]);
+                } else{
+                    flags.put(arg.substring(1),"");
+                }
+            }
+        }
+        return flags;
     }
 }
