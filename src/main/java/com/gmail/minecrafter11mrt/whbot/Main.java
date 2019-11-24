e89b269bc2730c2777d74366a0c9171385023b1a
underscore11code <minecrafter11mrt@gmail.com>
2019-11-23 20:40:58 -0800

    Revert "save"
    This reverts commit f08e0ebd75f3f2eff3b8b632b7e7950de1677742.

--- src/main/java/com/gmail/minecrafter11mrt/whbot/Main.java
+++ src/main/java/com/gmail/minecrafter11mrt/whbot/Main.java
@@ -1,19 +1,9 @@
 package com.gmail.minecrafter11mrt.whbot;
-
-import com.gmail.minecrafter11mrt.whbot.util.Parser;
-
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hey!");
         System.out.println("I'm a spigot plugin");
         System.out.println("Don't run me directly, put me in your plugin folder");
         if(args[0].equalsIgnoreCase("true")){new WHBot();}
-        if(args[1].equalsIgnoreCase("true")){
-            Parser parser=new Parser("-t:test -a");
-            HashMap<String,String> parseResults=parser.splitFlags();
-            parseResults.forEach();
-        }
     }
 }
--- src/main/java/com/gmail/minecrafter11mrt/whbot/util/Parser.java
+++ src/main/java/com/gmail/minecrafter11mrt/whbot/util/Parser.java
@@ -5,24 +5,14 @@ import java.util.HashMap;
 
 public class Parser {
     private String content;
-    public Parser(Message message){
+    Parser(Message message){
         content=message.getContent();
     }
-    public Parser(String text){
+    Parser(String text){
         content=text;
     }
-    public HashMap<String,String> splitFlags(){
-        HashMap<String,String> flags=new HashMap<>();
-        String[] argsSplit=content.split(" ");
-        for(String arg : argsSplit){
-            if(arg.startsWith("-")){
-                if(arg.contains(":")){
-                    flags.put(arg.split(":")[0],arg.split(":")[1]);
-                } else{
-                    flags.put(arg.substring(1),"");
-                }
-            }
-        }
-        return flags;
+    HashMap<String,String> splitFlags(){
+        HashMap<String,String> flags;
+
     }
 }
